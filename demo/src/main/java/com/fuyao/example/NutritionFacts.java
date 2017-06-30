4afe8d6ca6853f5c8e612076105eb05f57e7baa9
wangjuyang <juyang.wang@ant23.com>
2017-06-30 09:21:44 +0800

    提交

---
+++ demo/src/main/java/com/fuyao/example/NutritionFacts.java
@@ -0,0 +1,105 @@
+package com.fuyao.example;
+
+public class NutritionFacts {
+	private final int servingSize;
+
+    private final int servings;
+
+    private final int calories;
+
+    private final int fat;
+
+    private final int sodium;
+
+    private final int carbohydrate;
+
+     
+
+    public static class Builder{
+
+        //required parameters
+
+        private final int servingSize;
+
+        private final int servings;
+
+        //optional paramters-initialized to default values;
+
+        private int calories=0;
+
+        private int fat=0;
+
+        private int carbohydrate=0;
+
+        private int sodium=0;
+
+        public Builder(int servingSize,int servings){
+
+            this.servingSize=servingSize;
+
+            this.servings=servings;
+
+        }
+
+        public Builder calories(int val){
+
+            calories=val;
+
+            return this;
+
+        }
+
+        public Builder fat(int val){
+
+            this.fat=val;
+
+            return this;
+
+        }
+
+        public Builder carbohydrate(int val){
+
+            this.carbohydrate=val;
+
+            return this;
+
+        }
+
+        public Builder sodium(int val){
+
+            this.sodium=val;
+
+            return this;
+
+        }
+
+        public NutritionFacts build(){
+
+            return new NutritionFacts(this);
+
+        }
+
+    }
+
+    private NutritionFacts(Builder builder){
+
+        servingSize=builder.servingSize;
+
+        servings=builder.servings;
+
+        calories=builder.calories;
+
+        fat=builder.fat;
+
+        sodium=builder.sodium;
+
+        carbohydrate=builder.carbohydrate;
+
+    }
+
+    public static void main(String[] args){
+
+        NutritionFacts instance=new NutritionFacts.Builder(240, 8).calories(2).build();
+
+    }
+}
